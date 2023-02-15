b29dba6fea71c307b807ca306200cc15b315a74f
rishjs <rishika.a@juegostudioz.com>
2023-02-15 12:13:47 +0530

    hai

--- Day1.js
+++ Day1.js
@@ -1,17 +1,20 @@
 console.log('Hello');
-const args = process.argv.slice(2);
-
-args.forEach(arg => {
-  let envVar = process.env[arg];
-  if (envVar === undefined) {
-    console.error(`Could not find "${arg}" in environment`);
-  } else {
-    console.log(envVar);
-  }
-});
 
 const args = process.argv.slice(2);
 
 args.forEach(arg => {
   console.log(process.env[arg]);
 });
+
+
+const args1 = process.argv.slice(2);
+
+args1.forEach(arg => {
+  let envVar = process.env[arg];
+  if (envVar !== undefined) {
+    console.error(`Could not find "${arg}" in environment`);
+  } else {
+    console.log(envVar);
+  }
+});
+

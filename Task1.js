b0fdff250def747b1f3b034135587dc237e0321c
rishjs <rishika.a@juegostudioz.com>
2023-02-16 10:17:51 +0530

    second commit

--- Task1.js
+++ Task1.js
@@ -2,18 +2,14 @@
 //output -  {  
   //  ‘10/02/2023’: “MONDAY”,             ‘11/02/2023’: “TUESDAY”, 
      //       ‘12/02/2023’: “WEDNESDAY” } 
-
 let input_arr = new Array();
-input_arr = ["12/02/2023", "11/02/2023", "10/02/2023"] ;
-input_arr1 = ["Monday", "Tuesday", "Wednesday"] ;
-input_arr.reverse();
-
-
+input_arr = ["02/14/2023", "02/15/2023", "02/16/2023","02/17/2023"] ;
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let day=new Array();
 const obj = {}
-
-input_arr1.forEach((elem, i) => {
-   obj[`${input_arr[i]}`] = elem
+input_arr.forEach((elem, i) => {
+    const d = new Date(elem);
+    day[i] = weekday[d.getDay()];
+    obj[`${input_arr[i]}`] = elem
 })
-
-console.log(obj)
-
+ console.log(obj)
---
+++ util.js
@@ -0,0 +1,12 @@
+//
+// console.log('util.js');
+// const name="Andrew";
+// const add = function(a,b){
+//     return a+b;
+// }
+// module.exports = add;
+//
+const getNotes=function(){
+    return "Your notes...";
+}
+module.exports = getNotes;

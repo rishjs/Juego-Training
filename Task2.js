42d9285d2adcfed11e27bfcddd89b6f327a8df40
rishjs <125428593+rishjs@users.noreply.github.com>
2023-02-16 12:31:57 +0530

    Update Task2.js

--- Task2.js
+++ Task2.js
@@ -13,29 +13,63 @@
 //     user1.id='007';
 // console.log(user);
 
-const cloneDeep = require('lodash.clonedeep')
+// const user  = {
+//     id: '009',
+//     email: 'abc@linuxhint.com',
+//     contactInfo: {
+//     name: 'Harry',
+//     address: {
+//     city: 'Berlin',
+//     country: 'Germany'
+//     }
+//     }
+//     }
+//     const user1=user;
+//     user1.id='007';
+// console.log(user);
 
-const user  = {
-        id: '009',
-        email: 'abc@linuxhint.com',
-        contactInfo: {
-         name: 'Harry',
-         address: {
-         city: 'Berlin',
-        country: 'Germany'
-         }
-         }
-         }
+//const cloneDeep = require('lodash.clonedeep')
+
+// const user  = {
+//         id: '009',
+//         email: 'abc@linuxhint.com',
+//         contactInfo: {
+//          name: 'Harry',
+//          address: {
+//          city: 'Berlin',
+//         country: 'Germany'
+//          }
+//          }
+//          }
 
 
-let user1 = cloneDeep(user)
+//          const user1 = { ...user };
 
 
-user1.contactInfo.name = 'Rishika';
+// user1.contactInfo.name = 'yhn';
 
 
-console.log(user1)
+// console.log(user1)
 
 
 
-console.log(user)
+// console.log(user)
+
+const a = {
+    id: '009',
+     email: 'abc@linuxhint.com',
+    contactInfo: {
+    name: 'Harry',
+     address: {
+     city: 'Berlin',
+     country: 'Germany'
+     }
+    }
+  }
+  console.log(a);
+  console.log(a.contactInfo.name);  
+  const clone = JSON.parse(JSON.stringify(a));
+  clone.contactInfo.name = 'yhn';
+  console.log(clone);
+  console.log( clone.contactInfo.name); 
+  console.log( a); 

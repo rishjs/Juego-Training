ef7035b5020f81a8ea7b1feb5504036a0398d350
rishjs <125428593+rishjs@users.noreply.github.com>
2023-02-21 17:28:40 +0530

    Add files via upload

---
+++ Task3.js
@@ -0,0 +1,37 @@
+const request=require('request');
+const geocode=(limit=10,pageNumber=1,callback)=>{
+    const url='https://wizard-world-api.herokuapp.com/Elixirs';
+    request({url,json:true},(error,{body})=>{
+        if(error){
+            callback('Unable to connect to weather service',undefined)
+        }
+        else if(body.error){
+            callback('Unable to find the location',undefined)
+        }
+
+        else{
+            let start=(pageNumber*limit)-(limit-1);
+            let stop=pageNumber*limit;
+            callback(undefined,
+              { body,
+               start,
+               stop
+              }
+            )
+        }
+    })
+}
+const limit=process.argv[2];
+const pageNumber=process.argv[3];
+  geocode(limit,pageNumber,(error,data)=>{
+      if(error){
+          return console.log(error);
+      }
+      else{
+        for(let i=data.start;i<data.stop;i++)
+        {
+            console.log(data.body[i]);
+        }
+
+    } 
+  })

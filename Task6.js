da89d864b485fbc7bd8437927f06d77869bbd99c
rishjs <125428593+rishjs@users.noreply.github.com>
2023-02-22 17:10:13 +0530

    Add files via upload

---
+++ Task6.js
@@ -0,0 +1,63 @@
+const request=require('request');
+const task6=(limit=10,pageNumber=1,callback)=>{
+    const url='https://wizard-world-api.herokuapp.com/Elixirs';
+    request({url,json:true},(error,{body}={})=>{
+        if(error){
+            callback('Unable to connect to weather service',undefined)
+        }
+        else if(body.error){
+            callback('Unable to find the location',undefined)
+        }
+        else{
+            let start=(pageNumber*limit)-(limit-1);
+            let stop=pageNumber*limit;
+            callback(undefined,
+              { body,
+               start,
+               stop
+              }
+            )
+        }
+    })
+}
+const limit=process.argv[2];
+const pageNumber=process.argv[3];
+let flag=0;
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let name = "";
+rl.question("Enter the name\n", function (string) {
+  name = string;
+  let array=[];
+  task6(limit,pageNumber,(error,data)=>{
+      if(error){
+          return console.log(error);
+      }
+      else{
+        for(let i=data.start-1;i<data.stop;i++)
+        {
+            array.push(data.body[i]);   
+        }
+        search(array);
+    } 
+  })
+  rl.close();
+});
+function search(body){
+body.forEach(element => {
+
+    let newName = element.name;  
+     if(newName===name){
+        console.log(element);
+        flag=1;
+        return;
+     }
+});
+if(flag===0)
+{
+ console.log('No data available for this name');
+}
+}

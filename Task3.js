3ad48d538554673acc1df1ae12b18b18c0f7d856
rishjs <125428593+rishjs@users.noreply.github.com>
2023-02-22 13:30:58 +0530

    Update Task3.js

--- Task3.js
+++ Task3.js
@@ -1,3 +1,12 @@
+// 1.Paginate all the Data from API [https://wizard-world-api.herokuapp.com/Elixirs] with following.
+// There should be limit with parameter to specify the maximum items a call can have
+// If data set total items items is less that limit then total items should be returned
+// There should be an option to pass a Page number
+// If limit is not passed server should assume a limit of 10
+// If the page number is not passed it should be assumed as 1
+
+
+
 const request=require('request');
 const task4=(limit=10,pageNumber=1,callback)=>{
     const url='https://wizard-world-api.herokuapp.com/Elixirs';

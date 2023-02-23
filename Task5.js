11015b488a771d4f24f4718b99c350ee329bb2b2
rishjs <125428593+rishjs@users.noreply.github.com>
2023-02-23 11:31:48 +0530

    Update Task5.js

--- Task5.js
+++ Task5.js
@@ -1,51 +1,34 @@
 // Group all difficulty level in in one object.
-//If difficult level is null then in key NULL the data must be grouped  
+//If difficult level is null then in key NULL the data must be grouped  
 
 
 const request=require('request');
-const task5=(limit=10,pageNumber=1,callback)=>{
+let array=[];
+let keywords=['Advanced','poison', 'ringworm', 'portion', 'hair']
     const url='https://wizard-world-api.herokuapp.com/Elixirs';
     request({url,json:true},(error,{body}={})=>{
         if(error){
-            callback('Unable to connect to weather service',undefined)
+            console.log('Unable to connect to weather service')
         }
+
         else{
-            let start=(pageNumber*limit)-(limit-1);
-            let stop=pageNumber*limit;
-            callback(undefined,
-              { body,
-               start,
-               stop
-              }
-            )
+            for(let i=0;i<body.length;i++)
+            {
+              array.push(body[i]); 
+            }
+            groupBy(array);
         }
     })
-}
-const limit=process.argv[2];
-const pageNumber=process.argv[3];
-let array=[];
-  task5(limit,pageNumber,(error,data)=>{
-      if(error){
-          return console.log(error);
-      }
-      else{
-        for(let i=data.start-1;i<data.stop;i++)
-        {
-            array.push(data.body[i]);   
-        }
-       groupBy(array);
-    } 
-  })
-  function groupBy(data1){
-  let ObjMap ={};
-  data1.forEach(element => {
-    let makeKey = element.difficulty;
-     if(!ObjMap[makeKey]) {
-       ObjMap[makeKey] = [];
-     }
-    ObjMap[makeKey].push(Object.assign({},element));
-   });
-   console.log(ObjMap);
-}
- 
- 
+    function groupBy(data1){
+      let ObjMap ={};
+      data1.forEach(element => {
+        let makeKey = element.difficulty;
+         if(!ObjMap[makeKey]) {
+           ObjMap[makeKey] = [];
+         }
+        ObjMap[makeKey].push(Object.assign({},element));
+       });
+       console.log(ObjMap);
+    }
+     
+     

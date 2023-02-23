d61f44ae17684515f4a98a00983d5f57583ac9ad
rishjs <125428593+rishjs@users.noreply.github.com>
2023-02-23 11:27:11 +0530

    Update Task4.js

--- Task4.js
+++ Task4.js
@@ -4,60 +4,81 @@
 // In a string even if poisonous is included that full object should be returned.
 
 const request=require('request');
-const task4=(limit=10,pageNumber=1,callback)=>{
+let array=[];
+let keywords=['Advanced','poison', 'ringworm', 'portion', 'hair']
     const url='https://wizard-world-api.herokuapp.com/Elixirs';
     request({url,json:true},(error,{body}={})=>{
         if(error){
-            callback('Unable to connect to weather service',undefined)
+            console.log('Unable to connect to weather service')
         }
+
         else{
-            let start=(pageNumber*limit)-(limit-1);
-            let stop=pageNumber*limit;
-            callback(undefined,
-              { body,
-               start,
-               stop
-              }
-            )
+            for(let i=0;i<body.length;i++)
+            {
+                search(body[i]);
+            }
+            console.log(array)
         }
     })
-}
-const limit=process.argv[2];
-const pageNumber=process.argv[3];
+    function search(data){
+        let text=JSON.parse(JSON.stringify(data));
+                if(text["effect"]!=null)
+                {      
+                       
+                    let myArray = text["effect"].split(" ");
+                    for(let k=0;k<myArray.length;k++)
+                    {
+                        for(let u=0;u<keywords.length;u++){ 
+                        if(myArray[k].startsWith(keywords[u])){
+                            array.push(Object.assign({},data))
+                        }
+                    }
+                    }
+                
+                }
+        
+    }
+
+
+
+//seraching keywords in entire field
+const request=require('request');
 let array=[];
 let keywords=['Advanced','poison', 'ringworm', 'portion', 'hair']
-  task4(limit,pageNumber,(error,data)=>{
-      if(error){
-          return console.log(error);
-      }
-      else{
-        for(let i=data.start-1;i<data.stop;i++)
-        {
-            search(data.body[i]);
+    const url='https://wizard-world-api.herokuapp.com/Elixirs';
+    request({url,json:true},(error,{body}={})=>{
+        if(error){
+            console.log('Unable to connect to weather service')
         }
-        console.log(array)
-    } 
-  })
-  function search(data){
-    let text=JSON.parse(JSON.stringify(data));
-    for(let j in text){
-            if(text[j]!=null)
+
+        else{
+            for(let i=0;i<body.length;i++)
             {
-                if(typeof text[j]=="object") 
+                search(body[i]);
+            }
+            console.log(array)
+        }
+    })
+    function search(data){
+        let text=JSON.parse(JSON.stringify(data));
+        for(let j in text){
+                if(text[j]!=null)
                 {
-                    search(text[j]);
-                }         
-                else{          
-                let myArray = text[j].split(" ");
-                for(let k=0;k<myArray.length;k++)
-                {
-                    for(let u=0;u<keywords.length;u++){ 
-                    if(myArray[k].startsWith(keywords[u])){
-                        array.push(Object.assign({},data))
+                    if(typeof text[j]=="object") 
+                    {
+                        search(text[j]);
+                    }         
+                    else{          
+                    let myArray = text[j].split(" ");
+                    for(let k=0;k<myArray.length;k++)
+                    {
+                        for(let u=0;u<keywords.length;u++){ 
+                        if(myArray[k].startsWith(keywords[u])){
+                            array.push(Object.assign({},data))
+                        }
+                    }
                     }
                 }
                 }
-            }
-            }
-    } 
-}
+        } 
+    }
